eadb91315213a3338a3a6e606545c20d3701adc3
Marko Djokic <marko1319@its.edu.rs>
2020-12-30 21:33:36 +0100

    Add project files.

---
+++ ListeZad9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeZad9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> gradovi = new Dictionary<string, int>()
+            {
+                 {"Beograd", 2000000},
+                 {"Nis", 1400000 },
+                 {"Kraljevo", 400000 }
+            };
+            Console.WriteLine(gradovi.Remove("Nis"));
+            foreach (KeyValuePair<string, int> grad in gradovi)
+                Console.WriteLine(grad.Key + " - " + grad.Value);
+            Console.WriteLine("=====================================");
+
+            //pokusaj uklanjanja nepostojeceg elementa
+            Console.WriteLine(gradovi.Remove("Novi Sad"));
+            foreach (KeyValuePair<string, int> grad in gradovi)
+                Console.WriteLine(grad.Key + " - " + grad.Value);
+            Console.WriteLine("=====================================");
+
+            Console.ReadLine();
+        }
+    }
+}
